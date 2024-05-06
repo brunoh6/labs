d23caabdb121f04729abc62372c8efaa8ed0cb8f
Bruno <132168501+brunoh6@users.noreply.github.com>
2024-05-06 14:50:43 +0000

    fixed_code

--- 10thWeek/src/App.js
+++ 10thWeek/src/App.js
@@ -1,6 +1,7 @@
 import f3 from './images/f3.png';
 import f4 from './images/f4.png';
 import f5 from './images/f5.png';
+import ToDo from './components/ToDo';
 import './index.css';
 
 function App() {
@@ -8,21 +9,9 @@ function App() {
       <div className="container">
           <h2 className="main_title">What do you need?</h2>
           <div className="main_block">
-              <div className="blocks">
-                  <img src={f3} alt="pic"/>
-                      <h3>First block</h3>
-                      <p>Here goes text and more text and more text</p>
-              </div>
-              <div className="blocks">
-                  <img className="imagen" src={f4} alt="pic"/>
-                      <h3>Second block</h3>
-                      <p>Here goes text and more text and more text</p>
-              </div>
-              <div className="blocks">
-                  <img src={f5} alt="pic"/>
-                      <h3>Third block</h3>
-                      <p>Here goes text and more text and more text</p>
-              </div>
+              <ToDo title="First Block" img={f3} text='Here goes text and more and more text'/>
+              <ToDo title="Second Block" img={f4} text='Here goes text and more and more text'/>
+              <ToDo title="Third Block" img={f5} text='Here goes text and more and more text'/>
           </div>
       </div>
   );
