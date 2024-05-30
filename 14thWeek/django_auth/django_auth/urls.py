9a71a8b58299590482fd295ad12754718d1c5ec2
Bruno <132168501+brunoh6@users.noreply.github.com>
2024-05-30 12:52:47 +0000

    lab14

---
+++ 14thWeek/django_auth/django_auth/urls.py
@@ -0,0 +1,10 @@
+# django_auth/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('users.urls')),
+]
